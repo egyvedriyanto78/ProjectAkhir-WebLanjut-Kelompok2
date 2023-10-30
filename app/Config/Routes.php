661c9c55aa6b39494da109bfd91eadaf34bb34ce
sjuandira <salsajuandira04@gmail.com>
2023-10-31 01:12:27 +0700

    Update LandingPage

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,15 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-
+use App\Controllers\UserController;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/user/create', [UserController::class,'create']);
+$routes->get('/', [UserController::class,'index']);
+$routes->get('/home', [UserController::class,'index']);
+$routes->get('/about', [UserController::class,'about']);
+$routes->get('/service', [UserController::class,'service']);
+$routes->get('/team', [UserController::class,'doctor']);
+$routes->get('/appointment', [UserController::class,'appointment']);
+
---
+++ app/Views/appointment.php
@@ -0,0 +1,79 @@
+<?= $this->extend ('layouts/app-landing')?>
+<?= $this->section ('content') ?>          
+
+    <!-- Appointment Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <p class="d-inline-block border rounded-pill py-1 px-4">Pendaftaran</p>
+                    <h1 class="mb-4">Buat Janji Dengan Dokter Kami</h1>
+                    <p class="mb-4">Untuk menghindari penumpukan pasien, harap daftarkan diri anda terlebih dahulu</p>
+                    <div class="bg-light rounded d-flex align-items-center p-4 mb-3">
+                        <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
+                            <i class="fa fa-phone-alt text-primary"></i>
+                        </div>
+                        <div class="ms-4">
+                            <p class="mb-2">Hubungi Kami</p>
+                            <h5 class="mb-0">+012 345 6789</h5>
+                        </div>
+                    </div>
+                    <div class="bg-light rounded d-flex align-items-center p-4">
+                        <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style="width: 55px; height: 55px;">
+                            <i class="fa fa-envelope-open text-primary"></i>
+                        </div>
+                        <div class="ms-4">
+                            <p class="mb-2">E-mail kami</p>
+                            <h5 class="mb-0">klinikunila@gmail.com</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">                
+                    <div class="bg-light rounded h-90 d-flex align-items-center p-5">                        
+                        <form>                            
+                            <div class="row g-4">
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control border-0" placeholder="Nama Anda" style="height: 55px;">
+                                </div>                                
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control border-0" placeholder="Nomor Telepon" style="height: 55px;">
+                                </div>
+                                <!-- <div class="col-12 col-sm-6">
+                                    <select class="form-select border-0" style="height: 55px;">
+                                        <option selected>Choose Doctor</option>
+                                        <option value="1">Doctor 1</option>
+                                        <option value="2">Doctor 2</option>
+                                        <option value="3">Doctor 3</option>
+                                    </select>
+                                </div> -->
+                                <div class="col-12 col-sm-6">
+                                    <div class="date" id="date" data-target-input="nearest">
+                                        <input type="text"
+                                            class="form-control border-0 datetimepicker-input"
+                                            placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style="height: 55px;">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="time" id="time" data-target-input="nearest">
+                                        <input type="text"
+                                            class="form-control border-0 datetimepicker-input"
+                                            placeholder="Choose Date" data-target="#time" data-toggle="datetimepicker" style="height: 55px;">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <textarea class="form-control border-0" rows="5" placeholder="Describe your problem"></textarea>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100 py-3" type="submit">Book Appointment</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Appointment End -->
+        
+
+    <?= $this->endSection ('content') ?>
