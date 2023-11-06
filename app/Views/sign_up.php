ef7639219ce3592a394567ce2393f90c17b24e7f
sjuandira <salsajuandira04@gmail.com>
2023-11-06 20:22:55 +0700

    update database user

--- app/Views/sign_up.php
+++ app/Views/sign_up.php
@@ -1,34 +1,38 @@
 <?= $this->extend('layouts/app')?>
 <?= $this->section('content')?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
   <div class="container">
     <div class="card">
       <div class="card_title">
-        <h1>Create Account</h1>
-        <span>Already have an account? <a href="login">Sign In</a></span>
+        <h1>Buat Akun</h1>        
+        <span class="subtitle">Sudah memiliki akun? <a href="login">Masuk</a></span>        
+      </div>      
+      <div class="form">        
+      <form action="<?= base_url('/register/store')?>" method="post" enctype="multipart/form-data">           
+      <div class="input-control">
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email" id="email" class="form-control <?= (empty(validation_show_error('email')))? 
+        '':'is-invalid' ?>" value="<?= old('email') ?>">
+        <div class="msg invalid-feedback">
+            <?= validation_show_error('email') ?>
+        </div>
       </div>
-      <div class="form">
-      <form action="/register" method="post">
-        <input type="text" name="username" id="username" placeholder="UserName" />
-        <input type="email" name="email" placeholder="Email" id="email" />
-        <input type="password" name="password" placeholder="Password" id="password" />
-        <button>Sign Up</button>
+      <div class="input-control">
+        <label for="username">Nama Pengguna</label>
+        <input type="text" name="username" id="username" placeholder="Nama Pengguna" class="form-control <?= (empty(validation_show_error('username')))? 
+        '':'is-invalid' ?>" value="<?= old('username') ?>">
+        <div class=" msg invalid-feedback">
+            <?= validation_show_error('username') ?>
+        </div>
+        <div class="input-control">
+        <label for="password">Kata Sandi</label>
+        <input type="password" name="password" placeholder="Kata Sandi" id="password" class="form-control <?= (empty(validation_show_error('password')))? 
+        '':'is-invalid' ?>" value="<?= old('password') ?>">
+        <div class="msg invalid-feedback">
+            <?= validation_show_error('password') ?>
+        </div>
+        <button class="btn-signup">Buat Akun</button>      
         </form>
-      </div>
-      <div class="card_terms">
-          <input type="checkbox" name="" id="terms"> <span>I have read and agree to the <a href="">Terms of Service</a></span>
-      </div>
+      </div>      
     </div>
   </div>
-</body>
-</html>
 <?= $this->endSection('content') ?>
